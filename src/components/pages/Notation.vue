e217f1056653163b300d5c34563e53e4b651b1a3
Anthony AUDOIN <anthonyaudoin@orange.fr>
2022-03-28 10:57:52 +0200

    students view per class

--- src/components/pages/Notation.vue
+++ src/components/pages/Notation.vue
@@ -2,13 +2,16 @@
     <div>
         <div>
             <h1>{{ currentClassEvaluationId }}</h1>
-            <button>Élève précédent</button>
-            <button>Élève suivant</button>
+            {{students}}
+            <br><button @click="previousEleve">Élève précédent</button>
+            <button @click="nextEleve">Élève suivant</button>
         </div>
     </div>
 </template>
 
 <script>
+import DB from "../../Database.js";
+
 export default {
     name: "Notation",
     props: {
@@ -19,6 +22,30 @@ export default {
         },
     },
     data() {
+        return {
+        idEvaluation: this.currentClassEvaluationId,
+        idClasse: "",
+        students: Array,
+        counterEleve: 0,
+        }
+    },
+
+    methods: {
+        async nextEleve() {
+            this.counterEleve++;
+            const res = await DB.getAllClasses();
+            console.log(res[0]['students'][this.counterEleve]);
+            this.students = res[0]['students'][this.counterEleve];
+        },
+        async previousEleve() {
+            this.counterEleve--;
+            const res = await DB.getAllClasses();
+            console.log(res[0]['students'][this.counterEleve]);
+            this.students = res[0]['students'][this.counterEleve];
+        },
+        eleve() {
+            return this.id + this.counterEleve;
+        },
 
     },
 
